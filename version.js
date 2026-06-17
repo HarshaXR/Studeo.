ba1b9998bedb1475d12f9ab2e77022967cdae457
harsha <harsha.cherukuvada@gmail.com>
2026-06-17 16:01:57 +0530

    Create version.js
    Created a new version.js to enable updates for Studeo directly from the app.

---
+++ version.js
@@ -0,0 +1,5 @@
+{
+  "version": "0.5.0",
+  "releaseNotes": "Fixed issue with Hardware Acceleration on devices without a dedicated GPU.",
+  "downloadUrl": "https://github.com/HarshaXR/Studeo./releases/download/Release/Studeo-Beta-0.3.0.Setup.exe"
+}

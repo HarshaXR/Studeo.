0fbc845f9e1ec49504dba084c718873b508c69c5
harsha <harsha.cherukuvada@gmail.com>
2026-06-18 11:34:09 +0530

    Update version.js

--- Assets/version.js
+++ Assets/version.js
@@ -1,5 +1,5 @@
 {
   "version": "0.7.0",
   "releaseNotes": "Added a brand new OOBE experience for first time users of Studeo, and general improvements to the app.",
-  "downloadUrl": "https://github.com/HarshaXR/Studeo./releases/download/Release/Studeo-Beta-0.7.0.Setup.exe"
+  "downloadUrl": "https://github.com/HarshaXR/Studeo./releases/download/Beta-0.7.0/Studeo-Beta-0.7.0.Setup.exe"
 }

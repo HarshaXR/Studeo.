a5a2f317083ab848d2a583109a5925c482677f84
harsha <harsha.cherukuvada@gmail.com>
2026-06-25 01:06:39 +0530

    Update version.js

--- Assets/version.js
+++ Assets/version.js
@@ -1,5 +1,5 @@
 {
-  "version": "0.8.0",
-  "releaseNotes": "Introducing Wallpapers, Playlists, Clip to Notes, alongside more customisation features to make Studeo truly feel like yours. ",
-  "downloadUrl": "https://github.com/HarshaXR/Studeo./releases/download/Beta-0.8.0/Studeo-Beta-0.8.0.Setup.exe"
+  "version": "1.0.0",
+  "releaseNotes": "Meet the next generation of Studeo, now ready for the first release build. Upgrade now to the latest stable version. ",
+  "downloadUrl": "https://github.com/HarshaXR/Studeo./releases/latest/download/Studeo.Setup.exe"
 }
